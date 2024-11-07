035fd39f4dc24c2da55fac366d56d451829df7c3
hlebu5hek <74304719+hlebu5hek@users.noreply.github.com>
2024-11-07 18:20:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -21,30 +21,6 @@ def winner(b, l):
             (b[0][2] == l and b[1][1] == l and b[2][0] == l))
 
 
-def get_text(i, j, gb, l1, l2):
-    global sign
-    if board[i][j] == ' ':
-        if sign % 2 == 0:
-            l1.config(state=DISABLED)
-            l2.config(state=ACTIVE)
-            board[i][j] = "X"
-        else:
-            l2.config(state=DISABLED)
-            l1.config(state=ACTIVE)
-            board[i][j] = "O"
-        sign += 1
-        button[i][j].config(text=board[i][j])
-    if winner(board, "X"):
-        gb.destroy()
-        box = messagebox.showinfo("Winner", "Игрок 1 победил!")
-    elif winner(board, "O"):
-        gb.destroy()
-        box = messagebox.showinfo("Winner", "Игрок 2 победил")
-    elif (isfull()):
-        gb.destroy()
-        box = messagebox.showinfo("Tie Game", "Ничья")
-
-
 def isfree(i, j):
     return board[i][j] == " "
 
@@ -55,23 +31,6 @@ def isfull():
         if (i.count(' ') > 0):
             flag = False
     return flag
-
-
-def gameboard_pl(game_board, l1, l2):
-    global button
-    button = []
-    for i in range(3):
-        m = 3 + i
-        button.append(i)
-        button[i] = []
-        for j in range(3):
-            n = j
-            button[i].append(j)
-            get_t = partial(get_text, i, j, game_board, l1, l2)
-            button[i][j] = Button(
-                game_board, bd=5, command=get_t, height=4, width=8)
-            button[i][j].grid(row=m, column=n)
-    game_board.mainloop()
 
 
 def pc():
@@ -173,26 +132,11 @@ def withpc(game_board):
     gameboard_pc(game_board, l1, l2)
 
 
-def withplayer(game_board):
-    game_board.destroy()
-    game_board = Tk()
-    game_board.title("Крестики Нолики")
-    l1 = Button(game_board, text="Игрок 1 : X", width=10)
-
-    l1.grid(row=1, column=1)
-    l2 = Button(game_board, text="Игрок 2 : O",
-                width=10, state=DISABLED)
-
-    l2.grid(row=2, column=1)
-    gameboard_pl(game_board, l1, l2)
-
-
 def play():
     menu = Tk()
     menu.geometry("250x250")
     menu.title("Tic Tac Toe")
     wpc = partial(withpc, menu)
-    wpl = partial(withplayer, menu)
 
     head = Button(menu, text="---Крестики Нолики---",
                   activeforeground='white',
@@ -204,20 +148,14 @@ def play():
                 activebackground="yellow", bg="light blue",
                 fg="black", width=500, font='summer', bd=5)
 
-    B2 = Button(menu, text="Против Игрока", command=wpl, activeforeground='red',
-                activebackground="yellow", bg="light blue", fg="black",
-                width=500, font='summer', bd=5)
-
     B3 = Button(menu, text="Выход", command=menu.quit, activeforeground='red',
                 activebackground="yellow", bg="light blue", fg="black",
                 width=500, font='summer', bd=5)
     head.pack(side='top')
     B1.pack(side='top')
-    B2.pack(side='top')
     B3.pack(side='top')
     menu.mainloop()
 
 
-# Call main function
 if __name__ == '__main__':
     play()
